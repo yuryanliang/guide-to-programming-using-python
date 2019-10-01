7750d9411668c45ac9d4bb3be0dc6bec4582b934
Ryan Liang <yliang62@gmail.com>
2019-09-30 23:33:05 -0700

    chap2

--- Chap2/starbuzz.py
+++ Chap2/starbuzz.py
@@ -1,5 +1,14 @@
+import json
+import time
 import urllib.request
+price = 9999
+while price > 55:
+    page = urllib.request.urlopen('https://store.google.com/ca/config/pixel_3?hl=en-CA')
+    text = page.read().decode('utf8')
+    dict= json.loads(text)
+    index = text.find("$59")
+    price = int(text[index+1: index +3])
+    print(price)
+    time.sleep(2)
+print('buy')
 
-page = urllib.request.urlopen('http://www.beans-r-us.biz/prices.html')
-text = page.read().decode('utf8')
-print(text)
